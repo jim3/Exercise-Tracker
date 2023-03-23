2ba6b18530711035b60968f563efbb686a618fb5
jim3 <j3.casey@protonmail.com>
2023-03-23 19:43:33 -0400

    Update indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -5,7 +5,7 @@ const { getUserByName } = require("../database/indexDB");
 const { getAllUsers } = require("../database/indexDB");
 const { createOneUser } = require("../database/indexDB");
 const { createOneExercise } = require("../database/indexDB");
-const { getExerciseById } = require("../database/indexDB");
+const { getExercisesByUserId } = require("../database/indexDB");
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -62,66 +62,67 @@ router.get("/api/users/:userId/userbyid", async (req, res) => {
 
 // Create an exercise
 router.post("/api/users/:_id/exercises", async (req, res) => {
-    const userId = req.params._id; // grab id, query db for username
-    const result = await getUserById(userId);
+    const userId = req.params._id;
+    const result = await getUserById(userId); // find user by id
+    console.log(`Adding exercise for user with ID ${userId}`);
+    const exercise = req.body;
+
+    if (!exercise.description || !exercise.duration) {
+        return res.status(400).send("Description and duration are required");
+    }
+
+    // if date is empty, set it to today's date
+    if (exercise.date === "" || exercise.date === undefined) {
+        exercise.date = new Date().toISOString();
+    }
+    // if date is not empty, convert it to a date object
+    else {
+        let dateObj = new Date(exercise.date);
+
+        // check if date is valid
+        if (isNaN(dateObj.getTime())) {
+            return res.status(400).send("Invalid date");
+        }
+
+        // check if date is in the future
+        if (dateObj > new Date()) {
+            return res.status(400).send("Date cannot be in the future");
+        }
+
+        // set date to formatted string in UTC time zone
+        exercise.date = dateObj.toISOString();
+    }
+
+    const newExercise = {
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date,
+    };
+
+    await createOneExercise(userId, newExercise); // create exercise
+
+    const userExercises = await getExercisesByUserId(userId);
 
     const userObj = {
         username: result.username,
         _id: userId,
     };
 
-    // description/duration/[date]
-    const exercise = req.body;
-
-    // if date is empty, set it to today's date
-    if (exercise.date === "" || exercise.date === undefined) {
-        exercise.date = new Date().toLocaleDateString("en-us", {
-            weekday: "long",
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-        });
-    }
-
-    const exerciseObj = {
+    const exercises = userExercises.map((exercise) => ({
         description: exercise.description,
         duration: exercise.duration,
         date: exercise.date,
+    }));
+
+    const responseObj = {
+        username: userObj.username,
+        _id: userObj._id,
+        count: exercises.length,
+        log: exercises,
     };
 
-    const createExerciseObj = [
-        {
-            username: userObj.username,
-            description: exerciseObj.description,
-            duration: exerciseObj.duration,
-            date: exerciseObj.date,
-            _id: userObj._id,
-        },
-    ];
-
-    // fetch the exercise from the db [implicit return]
-    await createOneExercise(createExerciseObj);
-
-    // send the JSON response
-    res.json(createExerciseObj);
-});
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-// Get exercise by id
-router.get("/api/users/:_id/exercises/", async (req, res) => {
-    const userId = req.params._id;
-    const result = await getExerciseById(userId);
-
-    const exerciseObj = [
-        {
-            description: result.description,
-            duration: result.duration,
-            date: result.date,
-        },
-    ];
-
-    res.json(exerciseObj);
+    return res.json(responseObj);
+    // res.json(responseObj);
 });
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -129,31 +130,63 @@ router.get("/api/users/:_id/exercises/", async (req, res) => {
 // Get an exercise log
 router.get("/api/users/:_id/logs", async (req, res) => {
     const userId = req.params._id;
-    const user = await getUserById(userId); //  ->
+    const user = await getUserById(userId); // get username ->
+    console.log(`Retrieving exercise logs for user with ID ${userId}`);
+
+    // (*) Get all exercises by user id
+    let userExercises = await getExercisesByUserId(userId);
 
     const userObj = {
-        username: user.username,
-        _id: userId,
+        username: user.username, // username returned from db via getUserById
+        _id: userId, // id -> `:_id`
     };
 
-    // get exercise by id
-    const result = await getExerciseById(userId); // ->
-    const exerciseObj = [
-        {
-            description: result.description,
-            duration: result.duration,
-            date: result.date,
-        },
-    ];
+    // Get the optional query parameters
+    const { from, to, limit } = req.query;
 
-    const logObj = {
+    //
+    let fromDate = new Date(0);
+    let toDate = new Date();
+    let maxLogs = userExercises.length;
+
+    if (from) {
+        fromDate = new Date(from);
+    }
+
+    if (to) {
+        toDate = new Date(to);
+        toDate.setHours(23, 59, 59, 999); // set the time to the end of the day
+    }
+
+    if (limit) {
+        maxLogs = parseInt(limit);
+    }
+
+    fromDate.setHours(0, 0, 0, 0); // set the time to the beginning of the day
+
+    userExercises = userExercises
+        .filter((exercise) => {
+            const exerciseDate = new Date(exercise.date);
+            return exerciseDate >= fromDate && exerciseDate <= toDate;
+        })
+        .slice(0, maxLogs);
+
+    toDate.setHours(23, 59, 59, 999);
+
+    const exercises = userExercises.map((exercise) => ({
+        description: exercise.description,
+        duration: parseInt(exercise.duration),
+        date: exercise.date,
+    }));
+
+    const responseObj = {
         username: userObj.username,
         _id: userObj._id,
-        count: exerciseObj.length,
-        log: exerciseObj,
+        count: exercises.length,
+        log: exercises,
     };
 
-    res.json(logObj);
+    res.json(responseObj);
 });
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
